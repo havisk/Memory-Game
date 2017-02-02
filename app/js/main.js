9b8146efa09378bfaa1290e6f8f41949ed50e6c7
Kool Havis <havisk@gmail.com>
2017-02-01 22:55:28 -0500

    got cards showing

---
+++ app/js/main.js
@@ -0,0 +1,48 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// import $ from 'jquery';
+
+'use strict';
+
+var tileImage = ['illini.jpeg', 'indiana.jpg', 'Iowa.png', 'maryland.png', 'michstate.jpg', 'neb.jpg', 'northwestern.png', 'ohiostate.png', 'penn.gif', 'purdue.gif', 'Rutgers.jpg', 'terps.png', 'wis.png', 'Mich.jpg'];
+var board = document.getElementById("gameboard");
+
+var solutionArray = tileImage.concat(tileImage);
+// console.log(solutionArray);
+shuffleArray(solutionArray);
+console.log(solutionArray);
+
+// console.log(tileImage);
+start();
+function start() {
+  //clear board
+  board.innerHTML = " ";
+  //Create gameboard
+  for (var i = 0; i <= solutionArray.length - 1; i++) {
+    displayImage(i);
+    // console.log(i);
+  }
+}
+
+function displayImage(i) {
+  // console.log(i);
+  board.innerHTML += '<div class=" col-md-4 col-xs-3 card"><img src="images/big10.jpg" onclick="pickCard(\'' + solutionArray[i] + '\', \'' + i + '\',this); return false;" class="img"></div>';
+}
+
+window.pickCard = function (a, b, c) {
+  console.log(c);
+  c.src = 'images/' + solutionArray[i];
+};
+//shuffle tiles on board
+function shuffleArray(d) {
+  for (var c = d.length - 1; c > 0; c--) {
+    var b = Math.floor(Math.random() * (c + 1));
+    var a = d[c];
+    d[c] = d[b];
+    d[b] = a;
+  }
+  return d;
+}
+
+},{}]},{},[1])
+
+//# sourceMappingURL=main.js.map
--- js/main.js
+++ js/main.js
@@ -1,5 +1,44 @@
-import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
+// import $ from 'jquery';
 
-console.log('Hello, World');
+var tileImage = ['illini.jpeg', 'indiana.jpg', 'Iowa.png', 'maryland.png', 'michstate.jpg', 'neb.jpg',
+ 'northwestern.png', 'ohiostate.png', 'penn.gif', 'purdue.gif', 'Rutgers.jpg', 'terps.png', 'wis.png', 'Mich.jpg'];
+var board = document.getElementById("gameboard");
+
+var solutionArray = tileImage.concat(tileImage);
+// console.log(solutionArray);
+shuffleArray(solutionArray);
+console.log(solutionArray);
+
+
+// console.log(tileImage);
+start();
+function start(){
+  //clear board
+  board.innerHTML = " ";
+  //Create gameboard
+  for(var i = 0; i <= (solutionArray.length)-1; i++){
+    displayImage(i);
+    // console.log(i);
+  }
+
+}
+
+function displayImage(i) {
+  // console.log(i);
+  board.innerHTML += '<div class=" col-md-4 col-xs-3 card"><img src="images/big10.jpg" onclick="pickCard(\''+solutionArray[i]+'\', \''+i+'\',this); return false;" class="img"></div>';
+}
+
+window.pickCard = function(a,b,c){
+  console.log(c);
+  c.src = 'images/'+solutionArray[i];
+};
+//shuffle tiles on board
+function shuffleArray(d) {
+  for (var c = d.length - 1; c > 0; c--) {
+    var b = Math.floor(Math.random() * (c + 1));
+    var a = d[c];
+    d[c] = d[b];
+    d[b] = a;
+  }
+  return d;
+}
